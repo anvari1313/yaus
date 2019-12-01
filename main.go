246a9ae4f04fa6ed5b7f3a2a2143df0d93f34906
Ahmad Anvari <ahmad.anv@gmail.com>
2019-12-01 23:32:43 +0330

    Read config and setup app

--- main.go
+++ main.go
@@ -1,19 +1,11 @@
 package main
 
 import (
-	"fmt"
+	"github.com/anvari1313/yaus/app"
 	"github.com/anvari1313/yaus/config"
-	"net/http"
-
-	"github.com/labstack/echo/v4"
 )
 
 func main() {
 	c := config.ReadConfig("")
-	fmt.Println(c.Server.Addr)
-	e := echo.New()
-	e.GET("/", func(c echo.Context) error {
-		return c.String(http.StatusOK, "Hello, World!")
-	})
-	e.Logger.Fatal(e.Start(":1323"))
+	app.CreateApp(c)
 }
